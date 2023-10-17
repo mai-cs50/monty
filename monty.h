0e09b34d5a8042895f3f8f639f3d47bceacf6c9f
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-17 02:25:14 -0700

    s

--- monty.h
+++ monty.h
@@ -50,4 +50,22 @@ void find_func(char *, char *, int, int);
 
 stack_t *create_node(int n);
 void free_nodes(void);
-void print_stack(stack_t)
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
+
+void call_fun(op_func, char *, char *, int, int);
+
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t, unsigned int);
+
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
+
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
