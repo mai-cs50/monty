312c0acaf9f5e07e730978e911460821dc00ca3d
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-19 10:49:42 -0700

    s

--- monty.h
+++ monty.h
@@ -71,8 +71,8 @@ void print_char(stack_t **, unsigned int);
 void print_str(stack_t **, unsigned int);
 void rotl(stack_t **, unsigned int);
 
-void err(int err_code, ...);
-void more_err(int err_code, ...);
+void err(int error_code, ...);
+void more_err(int error_code, ...);
 void string_err(int error_code, ...);
 void rotr(stack_t **, unsigned int);
 
